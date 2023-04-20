54ff9098cd60cdc9c12d8bba636e9a179eeb018e
k34r <k34rrr@gmail.com>
2023-04-20 10:42:22 +0700

    modified

--- travel.js
+++ travel.js
@@ -1,16 +1,25 @@
-( function() {
+(function() {
 
 	const burger = document.querySelector('.header__burger');
 	const headernav = document.querySelector('.header__nav');
-	const navclose = document.querySelector('.heaeder__nav__close');
+	const navclose = document.querySelector('.header__nav__close');
+	const navElements = document.querySelectorAll('.header__nav__link');
 
-	burger.addEventListener('click', function () {
+	burger.addEventListener('click', function() {
 		headernav.classList.add('header__nav__mobile');
 	});
 
 	navclose.addEventListener('click', function () {
 		headernav.classList.remove('header__nav__mobile');
-	})
+	});
+
+	if(window.innerWidth < 768) {
+		for (let i=0; i<navElements.length; i++ ) {
+			navElements[i].addEventListener('click', function() {
+				headernav.classList.remove('header__nav__mobile');
+			});
+		}
+	}
 
 }());
 
@@ -25,3 +34,4 @@
 		}
 	}
 }());
+
